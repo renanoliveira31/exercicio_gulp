d2ea60e7237c6919ae361bd5ef829a2f33b35336
Renan Oliveira <renanfeoliveira1@gmail.com>
2024-03-14 15:45:04 -0300

    ajuste no arquivo gitignore

---
+++ build/scripts/main.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){console.log("Document ready"),$("#telefone").mask("(00) 0000-0000"),$("#cpf").mask("000.000.000-00"),$("#data").mask("00/00/0000"),console.log("Máscaras aplicadas")});
